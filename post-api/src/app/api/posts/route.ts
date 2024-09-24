b23c31e87bcf847ad72cb880444f888d6db7ecc4
Kenny Ho <kennyho.code@gmail.com>
2024-09-24 11:36:35 -0400

    api is barebones

--- post-api/src/app/api/posts/route.ts
+++ post-api/src/app/api/posts/route.ts
@@ -1,24 +1,97 @@
-// curl -X GET  http://localhost:3000/api/posts
-async function GET(request: Request) {
+import supabase from "@/utils/supabase";
+import { NextRequest } from "next/server";
+
+/*
+curl -X GET "http://localhost:3000/api/posts?id=1"
+*/
+async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const id = searchParams.get("id");
+
+  const getPostQuery = supabase.from("posts").select().eq("id", id);
+  const { data, error } = await getPostQuery;
+
+  // TODO: handle error
+
+  // TODO: return data
   return Response.json({ message: "sucess" });
 }
 
-// curl -X POST "https://api.example.com/users" \
-//      -H "Content-Type: application/json" \
-//      -d '{"name": "John Doe", "email": "john@example.com"}'
+/*
+curl -X POST "http://localhost:3000/api/posts" \
+-H "Content-Type: application/json" \
+-d '{
+  "title": "Post 1",
+  "content": "Some Random content",
+  "category": "cat1",
+  "tags": [
+    "cat1"
+  ]
+}'
+*/
 async function POST(request: Request) {
+  const body = await request.json();
+  const { title, content, category, tags } = body;
+
+  const { data, error } = await supabase
+    .from("posts")
+    .insert({ title, content, category, tags })
+    .select();
+
+  // TODO: do something with data
+
+  // TODO: handle error
   return Response.json({ message: "sucess" });
 }
 
-// curl -X PUT "http://localhost:3000/api/posts" \
-//      -H "Content-Type: application/json" \
-//      -d '{"name": "John Updated", "email": "john_updated@example.com"}
+/*
+curl -X PUT "http://localhost:3000/api/posts" \
+-H "Content-Type: application/json" \
+-d '{
+  "title": "Post 1 Updated",
+  "id": 1
+}'
+*/
+
 async function PUT(request: Request) {
+  const body = await request.json();
+  // only allow to update title for now
+  const { id, title: newTitle } = body;
+
+  console.log("newTitle", newTitle);
+
+  const { data, error } = await supabase
+    .from("posts")
+    .update({ title: newTitle })
+    .eq("id", id)
+    .select();
+
+  // TODO: do something with error
+  console.log("data: ", data);
+
+  // TODO: handle error
   return Response.json({ message: "sucess" });
 }
 
-// curl -X DELETE http://localhost:3000/api/posts
-async function DELETE(request: Request) {
+/*
+curl -X DELETE "http://localhost:3000/api/posts?id=1"
+*/
+
+async function DELETE(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const id = searchParams.get("id");
+
+  const { data, error } = await supabase
+    .from("posts")
+    .delete()
+    .eq("id", id)
+    .select();
+
+  // TODO: do something with data
+  console.log("data: ", data);
+
+  // TODO: handle error;
+
   return Response.json({ message: "sucess" });
 }
 
